324d78041e8dbe05d7e0c17aaa31d1794cacab7a
Joride <jvanasselt@macbookprojva.lan>
2014-12-25 09:33:09 +0100

    fixed an unrecognized selector on iOS7 when using the screen-controller

--- src/controller/UIDevice+MachineType.m
+++ src/controller/UIDevice+MachineType.m
@@ -19,30 +19,32 @@
      - heigth (pts) is the number of points required for a real-sized controller
      - width (pxs) is the number of pixels required for a real-sized controller
      - heigth (pxs) is the number of pixels required for a real-sized controller
+     NES Controller actual size is 12.497 cm x 5.512 cm
      
-                            |   ppi     cm/px       width	heigth  width   heigth
-                            |           w           (pts)   (pts)   (pxs)   (pxs)
-     --------------------------------------------------------------------------------------
-     iPhone 4 through 6 &   |
-     iPad mini retina       |   326     ~0,0077     802,5   354     1605	708
-     iPhone 6 Plus          |   401     ~0,0063     987     435,5   1974    871
-     iPad 1 and 2           |   132     ~0,0192     325     143,5	650     287
-     iPad retina            |   264     ~0,015625   800     353     1600    706 √
-     iPad mini              |   163     ~0,0155     400     177     803     354
+                            |   ppi     cm/px       width       heigth      width       heigth      scale
+                            |                       (pxs)       (pxs)       (pts)       (pts)       px/pts  fits?
+     -------------------------------------------------------------------------------------------------------------
+     iPhone 3.5" (480x320)  |   326     ~0,0077914   1603,95    707,45      802,00      353,5       2       NO
+     iPhone 4"   (548x320)  |   326     ~0,0077914   1603,95    707,45      802,00      353,5       2       NO
+     iPhone 6    (667x375)  |   326     ~0,0077914   1603,95    707,45      802,00      353,5       2       NO
+     iPhone 6 Plus (640x540)|   401     ~0,0063341   1972,95    870,20      657,67      290,0       3       NO
+     iPad 1 and 2 (1024x768)|   132     ~0,0192424   649,450    286,45      649,00      286,0       1       YES
+     iPad retina (1024x768) |   264     ~0,0096212  1298,900    572,90      649,5       286,5       2       YES
+     iPad mini (1024x768)   |   163     ~0,0155828   801,973    353,72      802,00      354,0       1       YES
+     iPad mini retina       |   326     ~0,0077914   1603,95    707,45      802,00      353,5       2       YES
+     (1024x768)
      */
 
     NSString * machineName = [self machineName];
  
     CGSize NESControllerSize = CGSizeZero;
     
-    if ([machineName containsString: @"iPhone"] ||
-        [machineName containsString: @"iPod"] ||
-        [machineName containsString: @"x86"])
+    if ([self machineIsIPhone])
     {
-        // as width, we return the width of the main screen
-        // the heigth is relative to that based on the
-        // NES Controllers actual size
-        // 12.497 cm / 5.512 cm
+        // No iPhone is big enough, so we return the biggest size possible
+        // that stil has the correct ratio.
+        // As width, we return the width of the main screen the heigth is
+        // relative to that based on the NES Controllers actual size
         CGFloat width = [UIScreen mainScreen].bounds.size.width;
         CGFloat heigth = width / 1152.0f * 443;
         
@@ -52,32 +54,112 @@
     {
         // this is an iPad, figure out what type and return the size based
         // on the above table
-        if ([machineName containsString: @"iPad2,5"] ||
-            [machineName containsString: @"iPad4,4"] ||
-            [machineName containsString: @"iPad4,5"])
+        if ([self machineIsIPadMini])
         {
             // iPad mini
-            // 803 * 354 pts
-            NESControllerSize = CGSizeMake(800.0f, 353.00f);
+            NESControllerSize = CGSizeMake(802.0f, 353.50f);
         }
         else
         {
             // large iPad
-            // 650 * 286.5 pts
-            NESControllerSize = CGSizeMake(650.0f, 286.5f);
-            
-            
+            NESControllerSize = CGSizeMake(649.5f, 286.5f);
         }
-        
     }
     
-    if (![machineName containsString: @"x86"])
+    // if we are not running on the simulatore, we should always have
+    // a non-zero size.
+    if ([machineName respondsToSelector: @selector(containsString:)])
     {
-        NSAssert(!CGSizeEqualToSize(CGSizeZero, NESControllerSize),
-                 @"No size for the controller found based on device type");
+        // iOS8
+        if (![machineName containsString: @"x86"])
+        {
+            NSAssert(!CGSizeEqualToSize(CGSizeZero, NESControllerSize),
+                     @"No size for the controller found based on device type");
+        }
+    }
+    else
+    {
+        // iOS7
+        NSRange x86 = [machineName rangeOfString: @"x86"];
+        if (x86.location == NSNotFound)
+        {
+            NSAssert(!CGSizeEqualToSize(CGSizeZero, NESControllerSize),
+                     @"No size for the controller found based on device type");
+        }
     }
     
     return NESControllerSize;
+}
++ (BOOL) machineIsIPhone
+{
+    NSString * machineName = [self machineName];
+    BOOL machineIsIPhone = NO;
+    if ([machineName respondsToSelector: @selector(containsString:)])
+    {
+        // iOS8
+        if ([machineName containsString: @"iPhone"] ||
+            [machineName containsString: @"iPod"] ||
+            [machineName containsString: @"x86"])
+        {
+            machineIsIPhone = YES;
+        }
+    }
+    else
+    {
+        // iOS7
+        NSRange iPhone = [machineName rangeOfString: @"iPad2,5"];
+        NSRange iPod = [machineName rangeOfString: @"iPad4,4"];
+        NSRange x86 = [machineName rangeOfString: @"iPad4,5"];
+        if (iPhone.location != NSNotFound ||
+            iPod.location   != NSNotFound ||
+            x86.location    != NSNotFound)
+        {
+            machineIsIPhone = YES;
+        }
+    }
+    return machineIsIPhone;
+}
++ (BOOL) machineIsIPadMini
+{
+    NSString * machineName = [self machineName];
+    BOOL machineIsIPadMini = NO;
+    
+    if ([machineName respondsToSelector: @selector(containsString:)])
+    {
+        // iOS8
+        if ([machineName containsString: @"iPad2,5"] ||
+            [machineName containsString: @"iPad4,4"] ||
+            [machineName containsString: @"iPad4,5"])
+        {
+            machineIsIPadMini = YES;
+        }
+    }
+    else
+    {
+        // iOS7
+        NSRange iPad25 = [machineName rangeOfString: @"iPad2,5"];
+        NSRange iPad44 = [machineName rangeOfString: @"iPad4,4"];
+        NSRange iPad45 = [machineName rangeOfString: @"iPad4,5"];
+        if (iPad25.location != NSNotFound ||
+            iPad44.location != NSNotFound ||
+            iPad45.location != NSNotFound)
+        {
+            machineIsIPadMini = YES;
+        }
+    }
+    
+    return machineIsIPadMini;
+}
++ (BOOL) machineIsIPad
+{
+    BOOL machineIsIPad = NO;
+    
+    if (![self machineIsIPadMini] &&
+        ![self machineIsIPhone])
+    {
+        machineIsIPad = YES;
+    }
+    return machineIsIPad;
 }
 
 +(NSString *)machineName
